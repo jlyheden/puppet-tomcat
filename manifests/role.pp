6570eea5f8045b590bd229d9aa8f94f9881f5a4a
jlyheden <johan@lyheden.com>
2013-02-19 01:18:30 +0100

    check for namevar

--- manifests/role.pp
+++ manifests/role.pp
@@ -9,6 +9,9 @@
 # tomcat::role { 'admin': }
 #
 define tomcat::role {
+  if size($name) == 0 {
+    fail("Namevar must not be empty.")
+  } 
   concat::fragment { "20_tomcat_users_xml_${name}":
     target  => $tomcat::tomcat_users_file,
     content => "<role rolename=\"${name}\"/>\n",

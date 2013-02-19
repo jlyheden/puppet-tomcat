20b8c401156cbe5769ed162e20feb0683cf789d9
jlyheden <johan@lyheden.com>
2013-02-19 01:18:49 +0100

    namevar and parameter validation

--- manifests/user.pp
+++ manifests/user.pp
@@ -19,6 +19,12 @@
 #
 define tomcat::user ( $password, $roles ) {
   validate_array($roles)
+  if size($roles) == 0 {
+    fail("Parameter roles must contain at least one element.")
+  }
+  if size($name) == 0 {
+    fail("Namevar must not be empty.")
+  }
   concat::fragment { "30_tomcat_users_xml_${name}":
     target  => $tomcat::tomcat_users_file,
     content => inline_template("<user username=\"<%= name %>\" password=\"<%= password %>\" roles=\"<%= roles.join(',') %>\"/>\n"),
